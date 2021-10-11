a2a2d61b4d1d4d1805bf252f3b33706033d25141
gheembona <kbeng88@gmail.com>
2021-10-11 22:19:19 +0900

    subLayout

--- resources/js/mainCustom.js
+++ resources/js/mainCustom.js
@@ -41,10 +41,69 @@ $(document).ready(function () {
 });
 
 $(window).on('load', function (){
-	var owl = $('#owlBannerFoot');
-	var prev = $('.main5 .brand-button-prev');
-	var next = $('.main5 .brand-button-next');
-	owl.owlCarousel({
+	var owl4 = $('#owlBannerSet4');
+	var prev4 = $('.main4 .brand-button-prev');
+	var next4 = $('.main4 .brand-button-next');
+	owl4.owlCarousel({
+    margin:35,
+    loop:true,
+    dots:false,
+   	//autoWidth:true,
+    items:3,
+		responsive:{
+			0:{
+				margin:15,
+				items:1,
+    		dots:true,
+			},
+			640:{
+				margin:20,
+				items:2,
+    		dots:true,
+			},
+			759:{
+    		dots:false
+			},
+			960:{
+				margin:20,
+				items:2,
+    		dots:false
+			},
+			1400:{
+				margin:35,
+				items:3,
+    		dots:false
+			}
+		},
+	});
+	
+	owl4.on('mousewheel', '.owl-stage', function (e) {
+    if (e.deltaY<0) {
+        owl4.trigger('next.owl');
+    } else {
+        owl4.trigger('prev.owl');
+    }
+    e.preventDefault();
+	});
+	
+	prev4.on('click',function(){
+		$('.main4 .owl-prev').click()
+	});
+	next4.on('click',function(){
+		$('.main4 .owl-next').click()
+	});
+	$('.btn_play').on('click',function(){
+		owl4.trigger('play.owl.autoplay',[1000])
+	});
+	$('.btn_stop').on('click',function(){
+		owl4.trigger('stop.owl.autoplay')
+	});
+
+
+	var owl5 = $('#owlBannerFoot');
+	var prev5 = $('.main5 .brand-button-prev');
+	var next5 = $('.main5 .brand-button-next');
+	owl5.owlCarousel({
     margin:35,
     loop:true,
     dots:false,
@@ -52,32 +111,31 @@ $(window).on('load', function (){
     items:5,
 	responsive:{
 		0:{
-			margin:20,
-			items:1,
+			margin:10,
+			items:2,
 		},
 		640:{
-			margin:20,
+			margin:10,
 			items:2,
 		},
 		759:{
 		},
 		960:{
-			margin:15,
+			margin:10,
 			items:2,
 		},
 		1400:{
-			margin:15,
-			items:2,
+			margin:25,
+			items:5,
 		}
 	},
-	})
-
-	prev.on('click',function(){
-		owl.trigger('prev.owl');
-	})
-	next.on('click',function(){
-		owl.trigger('next.owl');
-	})
+	});
+	prev5.on('click',function(){
+		$('.main5 .owl-prev').click()
+	});
+	next5.on('click',function(){
+		$('.main5 .owl-next').click()
+	});
 
 	
 });
